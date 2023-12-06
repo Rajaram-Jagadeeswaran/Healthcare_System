8f55bbf2075ff9c5080199c611b6a5eeeca8dfb5
EC2 Default User <ec2-user@ip-172-31-10-184.eu-west-1.compute.internal>
2023-12-06 10:18:29 +0000

    Updated version 1.6

--- users/views.py
+++ users/views.py
@@ -15,7 +15,7 @@ def sign_up(request):
             patient = Patient.objects.create(user=user, name=un)
 
             # Creating associated HealthRecord instance for the new user
-            health_record = HealthRecord.objects.create(user=user)
+            #health_record = HealthRecord.objects.create(user=user)
 
             messages.success(request, 'Account has been successfully created for {}.'.format(un))
             return redirect('/signin')
