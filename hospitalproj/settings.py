16369fd89d69e46ba3d518cf67f7b9e3f3805f11
EC2 Default User <ec2-user@ip-172-31-10-184.eu-west-1.compute.internal>
2023-12-11 17:18:15 +0000

    new version 4.5

--- hospitalproj/settings.py
+++ hospitalproj/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import secrets
+
+secretKey = secrets.token_hex(42)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(vn0kne-tx$1*-mk*!r#xw+#1p7ccp@c959roc#86lp=vm#nqx'
+SECRET_KEY = secretKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
